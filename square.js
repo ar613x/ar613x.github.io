592f8f69a17c1c256b6ec94d413f9ee301430761
ssdfour111 <tdasguptacbook18@gmail.com>
2026-06-01 19:36:45 -0400

    Fixed way too many dumb errors

--- square.js
+++ square.js
@@ -40,55 +40,37 @@ class Square {
     neighbors(cells,d=false,h=true,v=true) {
         let n = [];
         if (h) {
-            n.concat([cells[this.y][this.x-1], cells[this.y][this.x+1]]);
+            n = n.concat([cells[this.y][this.x-1], cells[this.y][this.x+1]]);
         }
         if (v) {
-            n.concat([cells[this.y-1][this.x], cells[this.y+1][this.x]]);
+            n = n.concat([cells[this.y-1][this.x], cells[this.y+1][this.x]]);
         }
         if (d) {
-            n.concat([cells[this.y-1][this.x-1], cells[this.y-1][this.x+1], cells[this.y+1][this.x+1], cells[this.y+1][this.x-1]]);
+            n = n.concat([cells[this.y-1][this.x-1], cells[this.y-1][this.x+1], cells[this.y+1][this.x+1], cells[this.y+1][this.x-1]]);
         }
-        for (let i of n) {
-            if (i.occ == false) {
-                n.splice(n.indexOf(i),1);
-            }
-        }
+        n = n.filter(i => i && i.occ);
         return n;
     }
     sames(n,di=false) {
-        for (let i of n) {
-            if (!di) {
-                if (i.occ && i.b == this.b) {
-                    continue;
-                } else {
-                    n.splice(n.indexOf(i),1);
-                }
-            } else {
-                if (i.occ && i.b != this.b) {
-                    continue;
-                } else {
-                    n.splice(n.indexOf(i),1);
-                }
-            }
-        }
+        n = n.filter(i => i && i.occ && di==true ? i.b == this.b : i.b !== this.b);
         return n;
     }
     hGroup(cells) {
-        let n = new Set();
+        let n = new Set([this]);
         for (let j of n) {
             j.sames(j.neighbors(cells,false,true,false)).forEach(k => n.add(k));
         }
         return Array.from(n);
     }
     vGroup(cells) {
-        let n = new Set();
+        let n = new Set([this]);
         for (let j of n) {
             j.sames(j.neighbors(cells,false,false,true)).forEach(k => n.add(k));
         }
         return Array.from(n);
     }
     dGroup(cells) {
-        let n = new Set();
+        let n = new Set([this]);
         for (let j of n) {
             j.sames(j.neighbors(cells,true,false,false)).forEach(k => n.add(k));
         }
@@ -103,9 +85,9 @@ function findGroup(cells, square) {
     function getMatches(cell, l) {
         if (!cell || l.has(cell)) return;
         l.add(cell);
-        let neighbors = square.same(square.neighbors(cells, cell));
+        let neighbors = cell.sames(cell.neighbors(cells, false, true, true), false);
         for (let neighbor of neighbors) {
-            getMatches(neighbor);
+            getMatches(neighbor, l);
         }
     }
     getMatches(square, group);
@@ -129,7 +111,7 @@ function allGroups(cells) {
 function groupSurrounded(cells,group) {
     let surrounded = true;
     for (let i of group) {
-        if (i.neighbors(cells,false).length != false) {
+        if (i.neighbors(cells,false).length !== false) {
             surrounded = false;
             break;
         }
@@ -157,17 +139,19 @@ function afterTurnRemoveCheck(cells) {
     }
 }
 function win(cells,cell) {
-    return cell.hGroup(cells).length >=5 || cell.vGroup(cells).length >= 5 || cell.dGroup(cells) >= 5;
+    return cell.hGroup(cells).length >=5 || cell.vGroup(cells).length >= 5 || cell.dGroup(cells).length >= 5;
 }
 function moveWinCheck(cells) {
     let newCells = structuredClone(cells);
-    for (let i of newCells) {
-        if (!i) {
-            continue;
-        }
-        if (win(newCells,i)) {
-            return true;
-            break;
+    for (let row of newCells) {
+        for (let i of row) {
+            if (!i) {
+                continue;
+            }
+            if (win(newCells,i)) {
+                return true;
+                break;
+            }
         }
     }
 }
