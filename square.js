a1fdb4cb7f5fb7ef6c10ea660436cec48bc30535
ssdfour111 <tdasguptacbook18@gmail.com>
2026-06-01 07:13:34 -0400

    Added some functions to the square class

--- square.js
+++ square.js
@@ -19,5 +19,36 @@ class Square {
             }
         }
     }
+    flip() {
+        if (this.occ) {
+            this.b = !this.b;
+        }
+    }
+    get() {
+        let c = 0;
+        if (this.occ) {
+            c = 1;
+            if (!this.b) {
+                c = 2;
+            }
+        }
+        return c;
+    }
+    neighbors(cells) {
+        return [cells[this.x - 1][this.y], cells[this.x + 1][this.y], cells[this.x][this.y - 1], cells[this.x][this.y + 1]];
+    }
+    sames(n) {
+        for (let i of n) {
+            if (i.occ && i.b == this.b) {
+                continue;
+            } else {
+                n.splice(n.indexOf(i), 1);
+            }
+        }
+        return n;
+    }
+    diagNeighbors(cells) {
+        return [cells[this.x-1][this.y-1], cells[this.x-1][this.y], cells[this.x-1][this.y+1], cells[this.x][this.y-1], cells[this.x][this.y+1], cells[this.x+1][this.y-1], cells[this.x+1][this.y], cells[this.x+1][this.y+1]];
+    }
 }
 export { Square };
