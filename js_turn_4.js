3c42e34f2bcf2dd5fcb3084c4b18a76ac56ee94a
ssdfour111 <tdasguptacbook18@gmail.com>
2026-06-21 19:07:26 -0400

    Started implementing 4 player
    Finished modifying square class and began implementing alliances

---
+++ js_turn_4.js
@@ -0,0 +1,103 @@
+import {Square, turnCheck, cellsUpdate, makeAlliance, alliances} from "./square4.js";
+// import {Piece, flipPieces} from "./piece.js";
+const board = document.getElementById("board");
+
+let lastCell = null;
+let sturn = 0; // 0 = black, 1 = white
+function updateTurnMarker(turn,v=false) {
+    let turnMarker = document.getElementById('turnmarker')
+    if (turn === 0) {
+        turnMarker.innerHTML =
+            `<span style='color:white;background:black;padding:4px;font-family:monospace;'>Black's ${v ? "victory" : "turn"}</span>`;
+    } else if (turn === 1) {
+        turnMarker.innerHTML =
+            `<span style='color:black;background:white;padding:4px;border:1px solid black;font:monospace;'>White's ${v ? "victory" : "turn"}</span>`;
+    }
+}
+function markLastMove(cell) {
+    if (lastCell) {
+        lastCell.classList.remove("last-move");
+    }
+
+    cell.classList.add("last-move");
+    lastCell = cell;
+}
+
+let cells = [];
+document.addEventListener("updateCell", (event) => {
+    const cell = document.getElementById(event.detail.id);
+    if (!cell) return;
+
+    let stone = cell.querySelector(".stone");
+    const color = event.detail.color;
+
+    if (color === 0) {
+        if (stone) cell.removeChild(stone);
+        return;
+    }
+
+    if (!stone) {
+        stone = document.createElement("div");
+        stone.classList.add("stone");
+        cell.appendChild(stone);
+    }
+
+    stone.className = color === 1 ? "stone black" : "stone white";
+    cellsUpdate(cells,event.detail.cell);
+});
+let victory = false;
+for (let row = 0; row < 19; row++) {
+    cells[row] = [];
+    for (let col = 0; col < 19; col++) {
+        const cell = document.createElement("div");
+        cell.className = "cell";
+        cells[row][col] = new Square(col, row, false, false);
+
+        cell.id = `${row},${col}`;
+        // Outline the center 7×7 area
+        if (row >= 6 && row <= 12) {
+            if (col === 6) cell.classList.add("center-left");
+            if (col === 12) cell.classList.add("center-right");
+        }
+
+        if (col >= 6 && col <= 12) {
+            if (row === 6) cell.classList.add("center-top");
+            if (row === 12) cell.classList.add("center-bottom");
+        }
+        
+        // Outline the center 3×3 area
+        if (row >= 8 && row <= 10) {
+            if (col === 8) cell.classList.add("center-left");
+            if (col === 10) cell.classList.add("center-right");
+        }
+
+        if (col >= 8 && col <= 10) {
+            if (row === 8) cell.classList.add("center-top");
+            if (row === 10) cell.classList.add("center-bottom");
+        }
+        cell.addEventListener("click", () => {
+            const stone = cell.querySelector(".stone");
+            let square = cells[row][col];
+
+            if (stone || victory) {
+                return;
+            }
+            if (sturn == 0) {
+                square.setColor(1);
+                sturn += 1;
+            } else {
+                square.setColor(2);
+                sturn -= 1;
+            }
+            markLastMove(cell);
+            const win = turnCheck(cells);
+            if (win) {
+                console.log(`Victory for ${sturn ? "black" : "white"}.`);
+                victory = true;
+            }
+            updateTurnMarker(victory ? 1-sturn : sturn,victory);
+        });
+        board.appendChild(cell);
+    }
+}
+updateTurnMarker(sturn);
---
+++ square4.js
@@ -0,0 +1,184 @@
+let alliances = new Map([
+    [1,[1]],
+    [2,[2]],
+    [3,[3]],
+    [4,[4]]
+]);
+function makeAlliance(all1,all2) {
+    let all1S = new Set(all1);
+    let all2S = new Set(all2);
+    for (let [i,o] of alliances) {
+        let allOS = new Set(o);
+        if (all1S.size === allOS.size && o.every(n => all1S.has(n))) {
+            alliances.set(i,o.concat(all2));
+            continue;
+        }
+        if (all2S.size === allOS.size && o.every(n => all2S.has(n))) {
+            alliances.set(i,o.concat(all1));
+            continue;
+        }
+    }
+}
+class Square {
+    constructor(x,y,col) {
+        this.x = x;
+        this.y = y;
+        this.col = col;
+    }
+    setColor(newCol) {
+        this.col = newCol;
+        let event = new CustomEvent('updateCell',{
+            bubbles: true,
+            detail: {
+                id=`${this.y},${this.x}`,
+                color: newCol,
+                cell: this
+            }
+        });
+        document.dispatchEvent(event);
+    }
+    neighbors(cells,d=false,h=true,v=true,d1=false) {
+        let n = [];
+        const safe = (x, y) => {
+            if (x < 0 || y < 0 || x > 18 || y > 18) return null;
+            return cells[y][x];
+        };
+        if (h) {
+            n = n.concat([safe(this.x - 1, this.y), safe(this.x + 1, this.y)]);
+        }
+        if (v) {
+            n = n.concat([safe(this.x, this.y - 1), safe(this.x, this.y + 1)]);
+        }
+        if (d) {
+            n = n.concat([safe(this.x + 1, this.y - 1), safe(this.x - 1, this.y + 1)]);
+        }
+        if (d1) {
+            n = n.concat([safe(this.x - 1, this.y - 1), safe(this.x + 1, this.y + 1)]);
+        }
+        return n.filter(i => i && i.col);
+    }
+    sames(n,di=false) {
+        for (let i = n.length - 1; i >= 0; i--) {
+            if (!di) {
+                if (!alliances.get(i.col).includes(n[i].col)) {
+                    n.splice(i, 1);
+                }
+            } else {
+                if (alliances.get(i.col).includes(n[i].col)) {
+                    n.splice(i, 1);
+                }
+            }
+        }
+        return n;
+    }
+    group(cells) {
+        let n = new Set([this]);
+        cells[this.y][this.x] = null;
+        for (let j of n) {
+            j.sames(j.neighbors(cells,false,true,true,false)).forEach(k => n.add(k));
+            for (let k of j.sames(j.neighbors(cells,false,true,true,false))) {
+                cells[k.y][k.x] = null;
+            }
+            if (n.size > 400) {
+                break;
+            }
+        }
+        return Array.from(n);
+    }
+    winGroup(cells, dir) {
+        let n = new Set([this]);
+        for (let j of n) {
+            j.sames(j.neighbors(cells,dir=="d",dir=="h",dir=="v",dir=="d1")).forEach(k => n.add(k));
+            if (n.size > 50) {
+                break;
+            }
+        }
+        return n;
+    }
+}
+function clone(arr) {
+    let newArr = arr.map(row =>
+    row.map(c => c ? new Square(c.x, c.y, c.occ, c.b) : null)
+    );
+    return newArr;
+}
+function allGroups(cells) {
+    let groups = [];
+    let newCells = clone(cells);
+    for (let j of newCells) {
+        for (let i of j) {
+            if (!i) {
+                continue;
+            }
+            groups.push(i.group(newCells));
+        }    
+    }
+    return groups
+}
+function groupColor(group) {
+    return group[0].color;
+}
+function groupSurrounded(cells,group) {
+    let newCol = groupColor(group);
+    for (let i of group) {
+        if (i.neighbors(cells,false,true,true,false).length !== 4-[i.x,18-i.x,i.y,18-i.y].filter(j => j === 18).length) {
+            return [false];
+        }
+        for (let j of i.neighbors) {
+            if (j.color !== newCol) {
+                newCol = j.color;
+            }
+        }
+    }
+    return [true,newCol];
+}
+function turnFlipCheck(cells) {
+    let groups = allGroups(cells);
+    let flips = new Map();
+    for (let i of groups) {
+        let surround = groupSurrounded(cells,i);
+        if (surround[0]) {
+            for (let j of i) {
+                flips.set(j,surround[1]);
+            }
+        }
+    }
+    for (let [i,o] of flips) {
+        i.setColor(o);
+    }
+}
+function win(cells,cell) {
+    let w = false;
+    for (let dir of ["h","v","d","d1"]) {
+        if (cell.winGroup(cells,dir).size >= 5) {
+            w = true;
+            break;
+        }
+    }
+    return w;
+}
+function moveWinCheck(cells) {
+    for (let row of cells) {
+        for (let i of row) {
+            if (!i) {
+                continue;
+            }
+            if (win(cells,i)) {
+                return true;
+                break;
+            }
+        }
+    }
+    return false;
+}
+function turnCheck(cells) {
+    turnFlipCheck(cells);
+    if (moveWinCheck(cells)) {
+        return true;
+    }
+    return false;
+}
+function cellsUpdate(cells,cell) {
+    cells[cell.y][cell.x] = cell;
+}
+export { Square, turnCheck, cellsUpdate, makeAlliance, alliances };
